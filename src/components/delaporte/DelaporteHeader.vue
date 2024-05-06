27c7f0dd51e2113a3d6325abf8057cc8371abcfb
criSiles <cristina.siles.vega@gmail.com>
2024-05-06 11:26:28 +0200

    Basic header done, there are little things to fix

--- src/components/delaporte/DelaporteHeader.vue
+++ src/components/delaporte/DelaporteHeader.vue
@@ -1,40 +1,41 @@
 <template>
   <section id="header">
-    <div class="header__container">
-      <div class="header_nav">=</div>
-      <div class="header__logo">
-        <img src="/src/assets/LogoDelaporte.png" alt="Delaporte logo" />
-      </div>
-      <ul class="social_icons">
-        <!-- TODO:  -->
-        <!-- <li>
+    <HamburgerComponent />
+    <div class="header_logo">
+      <img src="/src/assets/LogoDelaporte.png" alt="Delaporte logo" />
+    </div>
+    <ul class="social_icons">
+      <!-- TODO:  -->
+      <!-- <li>
                 <font-awesome-icon icon="fa-brands fa-spotify" />
               </li> -->
 
-        <!-- The X icon doesn't work -->
-        <!-- <li>
-            <i class="fab fa-x-twitter"></i>
-          </li> -->
-        <li>
-          <i class="fab fa-instagram"></i>
-        </li>
-        <li>
-          <i class="fab fa-facebook"></i>
-        </li>
-        <li>
-          <i class="fab fa-spotify"></i>
-        </li>
-        <li>
-          <i class="fab fa-youtube"></i>
-        </li>
-        <li>
+      <!-- The X icon doesn't work -->
+      <li>
+        <i class="fab fa-twitter"></i>
+      </li>
+      <li>
+        <i class="fab fa-instagram"></i>
+      </li>
+      <li>
+        <i class="fab fa-facebook"></i>
+      </li>
+      <li>
+        <i class="fab fa-spotify"></i>
+      </li>
+      <li>
+        <i class="fab fa-youtube"></i>
+      </li>
+      <!-- <li>
           <i class="fa fa-envelope"></i>
-        </li>
-      </ul>
-    </div>
+        </li> -->
+    </ul>
   </section>
 </template>
-<script>
+
+<script setup>
+import HamburgerComponent from '@/components/delaporte/HamburgerComponent.vue'
+
 // TODO: Why this doesn't work?
 
 // import { library } from '@fortawesome/fontawesome-svg-core'
@@ -49,21 +50,25 @@
 //   }
 // }
 </script>
-<style scoped>
+<style>
 #header {
   background-color: var(--bg-secondary);
   color: #fff;
+  margin-top: 0.2rem;
   padding: 1rem;
-}
-
-.header__container {
+  min-height: 66px;
+  height: 66px;
+  left: 0px;
+  right: 0px;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
-.header__logo img {
-  width: 100px;
+.header_logo img {
+  width: 140px;
+  max-height: 66px;
+  margin-left: 5.5rem;
 }
 
 .social_icons {
@@ -72,8 +77,6 @@
   color: #fff;
   list-style: none;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
   gap: 1rem;
 }
 </style>
---
+++ src/components/delaporte/HamburgerComponent.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="menu">
+    <ul
+      id="menu-items"
+      :style="{ transform: !isCheckboxChecked ? 'translateX(0)' : 'translateX(140%)' }"
+    >
+      <li><router-link :to="{ path: '/', hash: '#bio' }">Biografía</router-link></li>
+      <li><router-link :to="{ path: '/', hash: '#music' }">Música</router-link></li>
+      <li><router-link :to="{ path: '/', hash: '#tour' }">Tour</router-link></li>
+      <li><router-link :to="{ path: '/', hash: '#shop' }">Tienda</router-link></li>
+      <li><router-link :to="{ path: '/', hash: '#contact' }">Contacto</router-link></li>
+    </ul>
+    <div id="hamburger" class="menu-container menu-size">
+      <input
+        id="menu-checkbox"
+        class="absolute menu-size"
+        type="checkbox"
+        v-model="isCheckboxChecked"
+      />
+      <div id="hamburger-lines" class="absolute menu-size">
+        <span class="line line1"></span>
+        <span class="line line2"></span>
+        <span class="line line3"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isCheckboxChecked: false
+    }
+  }
+}
+</script>
+<style>
+#menu {
+  display: flex;
+  align-items: center;
+  font-family: 'Roboto', sans-serif;
+  text-transform: uppercase;
+}
+#menu-items {
+  background-color: var(--bg-secondary);
+  height: 100vh;
+  width: 100%;
+  padding-top: 10rem;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  text-align: center;
+  transition: transform 0.5s ease-in-out;
+  top: 0rem;
+  right: 138.5vw;
+  z-index: 2;
+}
+
+#menu-items li {
+  list-style: none;
+  margin-right: 1rem;
+  font-size: 2rem;
+  /* TODO: Why is not appying? */
+  color: #fff;
+  text-decoration: none;
+}
+
+/* TODO: Why is not appying? */
+#menu-items li:hover {
+  text-underline-offset: 0.6rem;
+  text-decoration-color: black;
+}
+
+#menu-checkbox {
+  display: inline-block;
+  cursor: pointer;
+  opacity: 0;
+  z-index: 4;
+}
+.menu-container {
+  margin: auto;
+  position: relative;
+}
+.menu-size {
+  width: 1rem;
+  height: 1rem;
+}
+.absolute {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+
+#hamburger-lines {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  cursor: pointer;
+  position: absolute;
+  z-index: 3;
+}
+
+#hamburger-lines .line {
+  display: block;
+  height: 0.15rem;
+  width: 100%;
+  border-radius: 0.625rem;
+  background: #fff;
+}
+
+#hamburger-lines .line1 {
+  transform-origin: 0% 0%;
+  transition: transform 0.4s ease-in-out;
+}
+
+#hamburger-lines .line2 {
+  transition: transform 0.2s ease-in-out;
+}
+
+#hamburger-lines .line3 {
+  transform-origin: 0% 100%;
+  transition: transform 0.4s ease-in-out;
+}
+</style>
